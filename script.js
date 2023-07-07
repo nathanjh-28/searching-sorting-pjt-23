9fd3f50bc8d7c776708dbac3484d23a08bbddcd7
nathanjh-28 <nathan@nathanjharris.com>
2023-07-07 13:07:09 -0400

    selection sort complete

--- script.js
+++ script.js
@@ -38,3 +38,23 @@ function bubbleSort(array) {
 /*
 Selection sort works by selecting the minimum value in a list and swapping it with the first value in the list. It then starts at the second position, selects the smallest value in the remaining list, and swaps it with the second element. It continues iterating through the list and swapping elements until it reaches the end of the list. Now the list is sorted. Selection sort has quadratic time complexity in all cases.
 */
+
+function selectionSort(array) {
+    let sortedLength = 0;
+    let currentMin = Infinity
+    let currentMinIdx = -1
+    while (sortedLength < array.length - 1) {
+        for (let i = sortedLength; i < array.length; i++) {
+            if (array[i] < currentMin) {
+                currentMin = array[i]
+                currentMinIdx = i;
+            }
+        }
+        swap(array, sortedLength, currentMinIdx)
+        currentMin = Infinity
+        currentMinIdx = -1
+        sortedLength++
+    }
+
+    return array;
+}
