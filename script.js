02d143f44388b8e2ad6416768a86aa7a5eee7a2f
nathanjh-28 <nathan@nathanjharris.com>
2023-07-07 12:41:34 -0400

    bubble sorted started

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log('sanity check')
+
+/*
+
+Bubble Sort
+
+Here we will see bubble sort. The bubble sort method starts at the beginning of an unsorted array and 'bubbles up' unsorted values towards the end, iterating through the array until it is completely sorted. It does this by comparing adjacent items and swapping them if they are out of order. The method continues looping through the array until no swaps occur at which point the array is sorted.
+
+This method requires multiple iterations through the array and for average and worst cases has quadratic time complexity. While simple, it is usually impractical in most situations.
+
+- Freecodecamp
+
+*/
